339c411f49a3ae2c7e25177097006a1058c94399
Connor Lee <connorlee77@gmail.com>
2019-04-30 16:49:56 -0700

    add file structure for python

---
+++ spacecraft-controller-python/Trajectory.py
@@ -0,0 +1,4 @@
+class Trajectory:
+
+	def __init__(self):
+		pass
---
+++ spacecraft-controller-python/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import Trajectory
+import Estimator
+import Controller
+import Dynamics 
+
+if __name__ == '__main__':
+
+	print('hello')
