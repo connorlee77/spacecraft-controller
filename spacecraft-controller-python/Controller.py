6efa05d843da7fd54c0e7babd9d827211958ad42
Connor Lee <connorlee77@gmail.com>
2019-04-30 17:29:38 -0700

    created controller and dynamics files

--- spacecraft-controller-python/Controller.py
+++ spacecraft-controller-python/Controller.py
@@ -1,4 +1,20 @@
+import numpy as np
+
 class Controller:
 
-	def __init__(self):
-		pass
+	# Kp - 3x3 matrix
+	# Kd - 3x# matrix
+	def __init__(self, Kp, Kd):
+		self.Kp = Kp
+		self.Kd = Kd
+
+	# all input 3x1 vectors
+	def u(self, xd, xdotd, x, xdot):
+		return -self.Kd*(xdot - xdotd) - self.Kp*(x - xd)
+
+def test():
+	ctrl = Controller(np.eye(3), np.eye(3))
+	print(ctrl.u(np.ones((3,1)), np.ones((3,1)), np.ones((3,1)), np.ones((3,1))))
+
+if __name__ == "__main__":
+    test()
